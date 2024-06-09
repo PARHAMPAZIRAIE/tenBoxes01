734db9eda4d3ed9cd0a06d561d896a84ff13be2b
PARHAMPAZIRAIE <parhampaziraie@gmail.com>
2024-06-08 22:46:18 -0700

    initial commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from . import db
+from datetime import datetime
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    box_id = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Comment
+from . import db
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/box/<int:box_id>', methods=['GET', 'POST'])
+def box(box_id):
+    if request.method == 'POST':
+        content = request.form['content']
+        comment = Comment(box_id=box_id, content=content)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('main.box', box_id=box_id))
+
+    comments = Comment.query.filter_by(box_id=box_id).all()
+    return render_template('box.html', box_id=box_id, comments=comments)
